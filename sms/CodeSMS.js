2350b039acbfc492b649dcea4d3c770c27adf0d8
lovelin0523 <18356201582@163.com>
2021-03-19 13:12:53 +0800

    更新

--- sms/CodeSMS.js
+++ sms/CodeSMS.js
@@ -1,37 +1,56 @@
+const tencentcloud = require("tencentcloud-sdk-nodejs");
+// 导入 SMS 模块的 client models
+const smsClient = tencentcloud.sms.v20190711.Client;
+const models = tencentcloud.sms.v20190711.Models;
+const Credential = tencentcloud.common.Credential;
+const ClientProfile = tencentcloud.common.ClientProfile;
+const HttpProfile = tencentcloud.common.HttpProfile;
 /**
- * 发送短信验证码
+ * 腾讯云短信验证码发送
  */
-var QcloudSms = require("qcloudsms_js");
 class CodeSMS {
-	constructor(phoneNumber, params) {
-		// 短信应用 SDK AppID
-		this.appid = 0000000;
-		// 短信应用 SDK AppKey
-		this.appkey = "";
-		// 短信模板 ID，需要在短信控制台中申请
-		this.templateId = 0000000;
-		// 签名，签名参数使用的是`签名内容`，而不是`签名ID`
-		this.smsSign = "";
-		//需要发送短信的手机号码
-		this.phoneNumber = phoneNumber;
-		//发送的数据数组
-		this.params = params;//验证码与过期时间
+	constructor() {
+		this.secretId = '';
+		this.secretKey = '';
+		this.SmsSdkAppid = ''; //sdk appId
+		this.Sign = ''; //短信签名内容
+		this.TemplateID = ''; //短信模板ID
 	}
 
-	send() {
-		return new Promise((resolve,reject)=>{
-			// 实例化 QcloudSms
-			var qcloudsms = QcloudSms(this.appid, this.appkey);
-			var ssender = qcloudsms.SmsSingleSender();
-			ssender.sendWithParam("86", this.phoneNumber, this.templateId, this.params, this.smsSign, "", "", function(err, res, resData){
-				resolve({
-					error:err,
-					res:res,
-					resData:resData
-				})
+	/**
+	 * 发送短信
+	 * @param {Object} phoneNumber 手机号
+	 * @param {Object} params 数据数组
+	 */
+	send(phoneNumber, params) {
+		return new Promise((resolve, reject) => {
+			//实例化一个认证对象，入参需要传入腾讯云账户密钥对 secretId 和 secretKey
+			let cred = new Credential(this.secretId, this.secretKey);
+			//实例化 SMS 的 client 对象
+			let client = new smsClient(cred, "ap-guangzhou");
+			//实例化一个请求对象，根据调用的接口和实际情况，可以进一步设置请求参数
+			let req = new models.AddSmsTemplateRequest();
+			//短信应用 ID:
+			req.SmsSdkAppid = this.SmsSdkAppid;
+			// 短信签名内容
+			req.Sign = this.Sign;
+			//下发手机号码，采用 e.164 标准，+[国家或地区码][手机号]
+			req.PhoneNumberSet = ["+86" + phoneNumber];
+			//模板 ID
+			req.TemplateID = this.TemplateID;
+			//模板参数
+			req.TemplateParamSet = params;
+			//通过 client 对象调用想要访问的接口
+			client.SendSms(req, function(err, response) {
+				// 请求异常返回，打印异常信息
+				if (err) {
+					reject(err)
+					return;
+				}
+				resolve(response)
 			});
 		})
 	}
 }
 
-module.exports = CodeSMS
+module.exports = new CodeSMS();
